5717ce8b0cedb9c2b4974375d83eb435f6488abe
Sledger <beefy1875@hotmail.co.uk>
2023-06-08 23:00:48 +0800

    ex 3.22 linted up

--- models/person.js
+++ models/person.js
@@ -7,36 +7,36 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected')
-    })
-    .catch(error => {
-        console.log('Error connecting to DB', error.message)
-    })
+  .then(() => {
+    console.log('connected')
+  })
+  .catch(error => {
+    console.log('Error connecting to DB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function(v) {
-                return  /^\d{2,3}-?\d{5,}$/.test(v)
-            }
-        }
+  name: {
+    type:String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return  /^\d{2,3}-?\d{5,}$/.test(v)
+      }
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -7,29 +7,29 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 
 if (process.argv.length === 3) {
-    console.log('phonebook:');
-    Person
-    .find({}) 
+  console.log('phonebook:')
+  Person
+    .find({})
     .then(persons => {
-        persons.forEach(p => console.log(p.name, p.number))
-        mongoose.connection.close()
+      persons.forEach(p => console.log(p.name, p.number))
+      mongoose.connection.close()
     })
 } else if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
-    
-    person.save().then(result => {
-        console.log('saved')
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save().then(() => {
+    console.log('saved')
+    mongoose.connection.close()
+  })
 }
